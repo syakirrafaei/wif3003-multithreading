9519e8770fe046ea4c6a6bb4f5b07968f1dd2464
syakirrafaei <syakir.rafaei@gmail.com>
2020-06-11 23:14:32 +0800

    Getting inputs, generating random double, and storing objects in ArrayList.

--- src/wif3003/game/WIF3003Game.java
+++ src/wif3003/game/WIF3003Game.java
@@ -1,9 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package wif3003.game;
+
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
 
 /**
  *
@@ -11,11 +13,53 @@ package wif3003.game;
  */
 public class WIF3003Game {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        //Variables Initialization
+        int n,m,t = 0;
+        double x,y = 0;
+        
+        //Create an instance of Scanner class
+        Scanner scan = new Scanner(System.in);
+        
+        //Create an array list containing the coordinate objects
+        List<Coordinate> coordinates = new ArrayList<>();
+        
+        //Create an instance of Random class
+        Random randomNumber = new Random();
+        
+        //Getting inputs from user
+        System.out.println("Please enter the number of coordinate, n: ");
+        n = scan.nextInt();
+        
+        System.out.println("How long will the program run in seconds, m: ");
+        m = scan.nextInt();
+        
+        System.out.println("How many threads should be used, t ");
+        t = scan.nextInt();
+        
+       for(int i=0; i<n; i++) {
+           //Generate random number from 0 to 1000
+           x = randomNumber.nextDouble()*1000; 
+           y = randomNumber.nextDouble()*1000; 
+           
+           //We have to make sure that there are no duplicate random number
+           
+          
+           //Create new object of Coordinate class
+           Coordinate myCoordinate = new Coordinate(x, y);
+           
+           //Add the object into the ArrayList coordinates
+           coordinates.add(myCoordinate);
+       }
+        
+        coordinates.forEach(coordinate -> {
+        System.out.println("The coordinate in x:" + coordinate.getX() + 
+                " and y:" + coordinate.getY());
+        });
+        
+        
+       
     }
     
 }

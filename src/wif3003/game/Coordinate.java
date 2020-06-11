7d08b08702722a6abdd835d929376e1440249786
syakirrafaei <syakir.rafaei@gmail.com>
2020-06-11 23:13:51 +0800

    Coordinate class to store x and y.

---
+++ src/wif3003/game/Coordinate.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wif3003.game;
+
+/**
+ *
+ * @author Syakir
+ */
+public class Coordinate {
+    private double x;
+    private double y;
+    
+    Coordinate(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    public double getX() {
+        return x;
+    }
+    
+    public double getY() {
+        return y;
+    }
+}

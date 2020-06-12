ec9f1898156ff02d30fd6231c4e033d293a4a877
syakirrafaei <syakir.rafaei@gmail.com>
2020-06-12 19:58:00 +0800

    Callable class for creating edge.

---
+++ src/wif3003/game/CreateEdge.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wif3003.game;
+
+import java.util.concurrent.Callable;
+
+/**
+ *
+ * @author Syakir
+ */
+public class CreateEdge implements Callable<Integer> {
+    @Override 
+    public Integer call() throws Exception {
+        Integer index = 0;
+        return index;
+    }
+}
